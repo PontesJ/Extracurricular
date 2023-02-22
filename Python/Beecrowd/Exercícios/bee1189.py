0b47b817977213fa8c030484954ac9b4bfe5f6c5
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2023-02-22 11:57:41 -0300

    bee1189

---
+++ Python/Beecrowd/Exercícios/bee1189.py
@@ -0,0 +1,31 @@
+caracter = input()
+
+matriz = []
+
+for i in range(12):
+    lin = []
+    for j in range(12):
+        lin.append(float(input()))
+    matriz.append(lin)
+
+if caracter == 'S':
+    soma = 0
+
+    for i in range(12):
+        for j in range(12):
+            if i + j < 11 and i > j:
+                soma += matriz[i][j]
+    print(f"{soma:.1f}")
+
+
+else:
+    soma = 0
+    cont = 0
+
+    for i in range(12):
+        for j in range(12):
+            if i + j < 11 and i > j:
+                soma += matriz[i][j]
+                cont += 1
+    media = soma / cont
+    print(f"{media:.1f}")
