ed6e517e02a275fb4afc44c6594271e57c2d334e
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2024-02-12 21:03:09 -0300

    Aula 15, 16 e 17 de php

---
+++ PHP/Udemy - Curso Completo de PHP 7/Aulas/Aula 17 - Conhecendo os Operadores do PHP/index04.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 55.0;
+$b = 55;
+
+var_dump($a > $b);
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+//var_dump($a = $b); //atribuição
+
+var_dump($a == $b); //comparação
+
+echo "<br>";
+
+var_dump($a === $b); //comparação de identidade
+
+echo "<br>";
+
+var_dump($a != $b); //diferente
+
+echo "<br>";
+
+var_dump($a !== $b); //diferente de identidade
