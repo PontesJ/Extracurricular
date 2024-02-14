6b7a67cf0cb5aea9fbf473da15d8322a71febd10
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2024-02-13 22:14:36 -0300

    Aula 18 de php

---
+++ PHP/Udemy - Curso Completo de PHP 7/Aulas/Aula 18 - Strings em PHP/index02.php
@@ -0,0 +1,27 @@
+<?php
+
+$nome = "joao rangel";
+
+$nome = strtoupper($nome);
+
+echo $nome;
+
+$nome = strtolower($nome);
+
+echo "<br>";
+
+echo $nome;
+
+echo "<br>";
+
+$nome = ucfirst($nome);
+
+echo $nome;
+
+echo "<br>";
+
+$nome = ucwords($nome);
+
+echo $nome;
+
+echo "<br>";
---
+++ PHP/Udemy - Curso Completo de PHP 7/Aulas/Aula 18 - Strings em PHP/index03.php
@@ -0,0 +1,13 @@
+<?php
+
+$empresa = "Hcode";
+
+$empresa = str_replace("o", "0", $empresa);
+
+echo $empresa;
+
+$empresa = str_replace("e", "3", $empresa);
+
+echo "<br>";
+
+echo $empresa;
